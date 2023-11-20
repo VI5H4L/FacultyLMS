0b8cd23c98720fa5baa786b51d107087b528d03a
jasraimanik <jasraimanik@gmail.com>
2023-11-20 16:29:27 +0530

    Login Page added

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 
 const connectDB = require("./config/connectDB");
-
 connectDB();
 
 app.get("/",(req,res)=>{
     res.send("Hello");
 })
+app.use('/login',require('./routes/login'));
 
 app.listen(PORT , () => {
     console.log(`Server started at ${PORT}`);
