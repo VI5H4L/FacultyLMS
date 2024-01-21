2c3b5a5b214328205974eadb691d611f08edc216
VishalAggarwal2 <136411362+VishalAggarwal2@users.noreply.github.com>
2024-01-21 16:11:02 +0530

    added position controller

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -4,20 +4,20 @@ const Faculty  = require('../model/Faculty')
 const verifyJWT = async (req, res, next) => {
 
     const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+    if (!authHeader?.startsWith('Bearer ')) return res.json(({message:"unauthorized here"}));
     const token = authHeader.split(' ')[1];
-    console.log(token)
+    console.log(token);
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
-            if (err) return res.sendStatus(403); //invalid token
+            if (err) return res.json(({message:"unauthorized"})) //invalid token
             req.email = decoded.UserInfo.email;
         }
     );
     const validUser = await Faculty.findOne({ email : req.email }).populate("leaves").exec();
     if (!validUser) {
-        return res.sendStatus(401);
+        return  res.json(({message:"unauthorized"}));
     }
     req.faculty = validUser;
     next();
--- frontend/src/Context/contextProvider.js
+++ frontend/src/Context/contextProvider.js
@@ -1,28 +1,54 @@
 // MyContextProvider.js
 import React, { useState,useContext } from "react";
 
-
+import Cookies from 'js-cookie'
 import MyContext from "./createContext";
 const MyContextProvider = ({ children }) => {
 const[initial,setInitial]=useState("context value");
-
+const token = Cookies.get('jwt');
+const findPos=async()=>{
+  const token = localStorage.getItem('factoken');
+  const obj = {"token":token};
+  const response= await fetch('http://localhost:3000/login/sendpos', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+  const resJson = await response.json();
+  console.log("pos called");
+  console.log(resJson);
+  localStorage.setItem("pos",resJson.position);
+  }
+  
 const dataSend=async(path,obj)=>{
   try{
     const response = await fetch('http://localhost:3000/login', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
-          // Add any additional headers if needed
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify(obj),
       });
   const resJson = await response.json();
+// Extract cookies from the response headers
+const cookies = response.headers.get('jwt');
+console.log(token);
+// Save the cookies to the document.cookie
+if (cookies) {
+  document.cookie = cookies;
+}
+localStorage.setItem('factoken',resJson.accessToken);
   console.log(resJson);
+ await findPos();
   }catch(e){
     console.log("issue occured");
   }
 
 }
+
 
 
 const obj ={initial,setInitial,dataSend}
