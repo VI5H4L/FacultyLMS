3252ef7139fb1b179b371c42b742b114b3fa05a8
Srinjoy1604 <138678776+Srinjoy1604@users.noreply.github.com>
2024-02-01 20:22:34 +0530

    UI changes in admin dashboard

--- frontend/src/components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard/AdminDashboard.jsx
@@ -3,13 +3,14 @@ import styles from './AdminDashboard.module.css';
 import ReqBar from "../RequestBar/RequestBar";
 function ADash()
 {
-    const LeaveReq=[
-        {id:"1",name:"Faculty1",LeaveType:"CL",PostingDate:"10/01/2024",status:"pending"},
-        {id:"2",name:"Faculty2",LeaveType:"PL",PostingDate:"15/01/2024",status:"pending"}
-    ];
+    // var LeaveReq=[
+    //     {id:"#",name:"Faculty Name",LeaveType:"Leave Type",PostingDate:"Posting Date",status:"Status"}
+    // ];
+    var LeaveReq=[{id:"1",name:"Faculty1",LeaveType:"CL",PostingDate:"10/01/2024",status:"pending",action:"action"},
+    {id:"2",name:"Faculty2",LeaveType:"PL",PostingDate:"15/01/2024",status:"pending",action:"action"}]
     const listItem= LeaveReq.map((item)=>{
         return(
-            <ReqBar id={item.id} name={item.name} lt={item.LeaveType} pd={item.PostingDate} st={item.status}/>
+            <ReqBar id={item.id} name={item.name} lt={item.LeaveType} pd={item.PostingDate} st={item.status} act={item.action}/>
         );
     });
     return(
@@ -21,18 +22,21 @@ function ADash()
             </div> */}
             <div className={`${styles.ADS}`} style={{width:"64%",paddingLeft:"2%",paddingRight:"2%"}}>
                 <div><h4 className={`${styles.htxt}`}>LATEST LEAVE APPLICATIONS</h4></div>
-                <div className={`${styles.table}`}>
-                    <div>#</div>
-                    <div>Faculty Name</div>
-                    <div>Leave Type</div>
-                    <div>Posting Date</div>
-                    <div>Status</div>
-                    <div>Action</div>
-               </div>
+                
+                    <div className={`${styles.table}`}>
+                        <th>#</th>
+                        <th>Faculty Name</th>
+                        <th>Leave Type</th>
+                        <th>Posting Date</th>
+                        <th>Status</th>
+                        <th>Details</th>
+                    </div>
+                    <div className={`${styles.reqbar}`}>
+                        {listItem}
+                    </div>
+            
             </div>
-            <div className={`${styles.reqbar}`}>
-                {listItem}
-            </div>
+            
 
         </div>
     );
--- frontend/src/components/RequestBar/RequestBar.jsx
+++ frontend/src/components/RequestBar/RequestBar.jsx
@@ -4,18 +4,36 @@ import styles from './RequestBar.module.css';
 function ReqBar(props)
 {
     return(
-        <div className={`${styles.bars}`}>
-            <h3>{props.id}</h3>
-            <h3>{props.name}</h3>
-            <h3>{props.lt}</h3>
-            <h3>{props.pd}</h3>
-            <h3>{props.st}</h3>
-            <form className={`${styles.selection_panel}`}>
-                <div className={`${styles.approve}`}><input type="radio"   id="choice" name="action" value="yes" />Approve</div>
+        
+                
+        <tr className={`${styles.bars}`}>
+                    <td>
+                        <h3>{props.id}</h3>
+                    </td>
+                    <td>
+                        <h3>{props.name}</h3>
+                    </td>
+                    <td>
+                        <h3>{props.lt}</h3>
+                    </td>
+                    <td>
+                        <h3>{props.pd}</h3>
+                    </td>
+                    <td>
+                        <h3>{props.st}</h3>
+                    </td>
+                    <td>
+                        <button>{props.act}</button>
+                    </td>
 
-                <div className={`${styles.deny}`}><input type="radio"   id="choice" name="action" value="no" />Deny</div>
-            </form>
-        </div>
+                                    {/*<form className={`${styles.selection_panel}`}>
+                    <div className={`${styles.approve}`}><input type="radio"   id="choice" name="action" value="yes" />Approve</div>
+                    <div className={`${styles.deny}`}><input type="radio"   id="choice" name="action" value="no" />Deny</div>
+                </form>*/}
+                <div style={{position:"absolute"}} className={`${styles.popup}`}> <h3>I am Harshit</h3></div>
+        </tr>
+
+            
     );
 }
 export default ReqBar;
